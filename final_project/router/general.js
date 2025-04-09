2cf31f19063bd6e2b223c7d984db1a511f2ef079
matthewserafica <matthewserafica01@gmail.com>
2025-04-08 21:08:09 -0400

    Update general.js task 1-4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,61 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookList = JSON.stringify(books, null, 2);
+  return res.status(200).send(bookList);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+    const bookDetails = JSON.stringify(books[isbn], null, 2);
+    return res.status(200).send(bookDetails);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author.toLowerCase();
+  const matchingBooks = [];
+
+  for (const isbn in books) {
+    if (books[isbn].author.toLowerCase() === author) {
+        matchingBooks.push({isbn, ...books[isbn]});
+    }
+  }
+
+  if (matchingBooks.length > 0) {
+    const response = JSON.stringify(matchingBooks, null, 2);
+    return res.status(200).send(response);
+  } else {
+    return res.status(404).json({message: "No books found by this author"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const matchingBooks = [];
+
+  for (const isbn in books) {
+    if (books[isbn].title.toLowerCase() === title) {
+        matchingBooks.push({isbn, ...books[isbn]});
+    }
+   }
+
+   if (matchingBooks.length > 0) {
+    const response = JSON.stringify(matchingBooks, null, 2);
+    return res.status(200).send(response);
+   } else {
+    return res.status(404).json({message: "No books found by this title"});
+   }
 });
 
 //  Get book review
