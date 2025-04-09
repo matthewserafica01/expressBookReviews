3cde1c940a0f4822a772834dd086d0b4a331413e
matthewserafica <matthewserafica01@gmail.com>
2025-04-08 20:42:29 -0400

    Update index.js and auth_users.js for authentication mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { isValid, users } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,8 +12,36 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.session.authorization && req.session.authorization.accessToken;
+
+    if (!token) {
+        return res.status(403).json({message: "User not logged in"});
+    }
+
+    jwt.verify(token, "access", (err, user) => {
+        if (err) return res.status(403).json({message: "Invalid token"});
+
+        req.user = user;
+        next();
+    });
 });
+
+app.post("/register", (req, res) => {
+    const {username, password} = req.body;
+
+    if (!isValid(username)) {
+        return res.status(400).json({message: "Invalid username"});
+    }
+
+    const userExists = users.some((u) => u.username === username);
+
+    if (userExists) {
+        return res.status(400).json({message: "User already exists"});
+    }
+
+    users.push({username, password});
+    res.status(201).json({message: "User registered successfully"});
+})
  
 const PORT =5000;
 
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,39 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+  return username && typeof username === "string";
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password} = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and Password are required"});
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({message: "Invalid Credentials"});
+  }
+
+  let accessToken = jwt.sign({username}, "access", {expiresIn: "1h"});
+  req.session.authorization = { accessToken};
+
+  return res.status(200).json({ message: "Login successful", token: accessToken });
 });
 
 // Add a book review
