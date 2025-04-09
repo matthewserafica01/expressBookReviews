7c9d856e6f1cbdbc5f97eb0b86828b14ea432ad5
matthewserafica <matthewserafica01@gmail.com>
2025-04-08 21:29:42 -0400

    Update auth_users.js and general.js task 6-9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -45,7 +45,29 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.body;
+  const username = req.user.username;
+
+  if (books[isbn]) {
+    books[isbn].reviews = books[isbn].reviews || {};
+    books[isbn].reviews[username] = review;
+    return res.status(200).json({message: "Review added", reviews: books[isbn].reviews});
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.user.username;
+
+    if (books[isbn]) {
+        delete books[isbn].reviews[username];
+        return res.status(200).json({message: "Review deleted successfully"})
+    } else {
+        return res.status(404).json({message: "Book not found"});
+    }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,7 +73,14 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  
+  if (books[isbn]) {
+    const reviews = JSON.stringify(books[isbn].reviews, null, 2);
+    return res.status(200).send(reviews);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.general = public_users;
